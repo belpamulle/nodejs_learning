ae8d05761983148e8d1c71cdee5a85e7741fdfb0
Gayan Belpamulle <gayan.belpamulle@digio.com.au>
2018-09-06 16:13:06 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+function placeOrder(orderNumner){
+    console.log('Order number'+orderNumner+ ' placed.');
+
+    getOrder(function (){
+        console.log(orderNumner+' Order deliovered');
+    });
+
+
+}
+
+function getOrder(callback){
+    setTimeout(callback,3000);
+}
+
+
+placeOrder(1);
+placeOrder(2);
+
+
+var myfunObject = {
+       funcA : function(){ 
+           console.log('or this is crazy');
+        }
+};
+
+myfunObject.funcA();
+
+
+var objectbasefunc = {
+    firstfunc : function(){
+            console.log((this===objectbasefunc) + '');
+    }
+};
+
+objectbasefunc.firstfunc();
+
+
+function player(){
+    this.name = '';
+    this.points = 100;
+    this.givePoints = function(targetPlayer){
+            targetPlayer.points= targetPlayer.points+1;
+            this.points= this.points-1;
+            console.log(this.name+' gave 1 point to '+ targetPlayer.name);
+    };
+};
+
+var gayan = new player();
+gayan.name= 'Gayan';
+var nayomi = new player();
+nayomi.name= 'Nayomi';
+gayan.givePoints(nayomi);
+console.log(gayan.points);
+console.log(nayomi.points);
+
+
+player.prototype.myuppercutFunction = function (counterplayer){
+    counterplayer.points -=3;
+    console.log(counterplayer.name +'\'s new points = '+counterplayer.points);
+}
+
+nayomi.myuppercutFunction(gayan);
